2c5cde7457a0570e4d6056b733ca56cf756b2d17
Nolan Darilek <nolan@thewordnerd.info>
2019-09-09 15:03:48 -0500

    Initial commit.

---
+++ scenes/Main.gd
@@ -0,0 +1,13 @@
+extends Node
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	Globals.tts.speak("Hello, world.", true)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
