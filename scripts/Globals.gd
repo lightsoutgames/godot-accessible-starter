e715474825042d1bd6676e130ae9f8cba01bb6ee
Nolan Darilek <nolan@thewordnerd.info>
2019-09-18 10:16:10 -0500

    Update to latest godot-accessibility and fix GitLab redirect.

--- scripts/Globals.gd
+++ scripts/Globals.gd
@@ -1,6 +1,6 @@
 extends Node
 
-const TTS = preload("res://addons/godot-tts/godot-tts.gdns")
+const TTS = preload("res://addons/godot-tts/TTS.gd")
 var tts
 
 # Called when the node enters the scene tree for the first time.
