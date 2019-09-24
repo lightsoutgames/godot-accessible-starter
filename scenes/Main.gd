251ee359d46fe42ac76de6b01cbaa5da673a08da
Nolan Darilek <nolan@thewordnerd.info>
2019-09-24 14:22:15 -0500

    Make `TTS` a singleton and load directly from the plugin.

--- scenes/Main.gd
+++ scenes/Main.gd
@@ -6,7 +6,7 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	Globals.tts.speak("Hello, world.", true)
+	TTS.speak("Hello, world.")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
--- scripts/Globals.gd
+++
@@ -1,8 +0,0 @@
-extends Node
-
-const TTS = preload("res://addons/godot-tts/TTS.gd")
-var tts
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-    tts = TTS.new()
